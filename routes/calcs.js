c2af59afc4e6de2146a01a7bf897c371af414737
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-03-07 23:00:00 -0400

    add: func `fat` para calcular o fatorial

--- routes/calcs.js
+++ routes/calcs.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 
-function fat (n) { // TODO
-  return n;
-}
+/**
+ * Calcula o fatorial de um número natural.
+ * Se o número passado não for um inteiro positivo,
+ * o erro `RangeError` é lançado.
+ * @param {number} n O número alvo da computação.
+ * @returns {number} O fatorial de `n`.
+ */
+const fat = (function fat() {
+  const valoresComputados = {0:1, 1:1}; // cache
+  return calcFatComMemoizacao;
+
+  function calcFatComMemoizacao(n) {
+    if (n < 0 || !Number.isInteger(n)) {
+      throw RangeError(`O valor passado ('${n}') é negativo ou não inteiro.`);
+    }
+
+    return (valoresComputados[n])
+         ? valoresComputados[n]
+         : valoresComputados[n] = n * calcFatComMemoizacao(n - 1);
+  }
+}());
 
 router.post('/fat', (req, res) => {
   const {n} = req.body;
