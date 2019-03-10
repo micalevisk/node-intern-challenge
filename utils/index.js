e6abc969f248a4c29dcaa61e62f8a0db11d25fd3
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-03-10 19:04:04 -0400

    add: func wrapper para rotas assíncronas

---
+++ utils/index.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * Wrapper para handlers de rotas assíncronos.
+ * Assim, não será preciso inserir clásulas `try..catch`
+ * nos handlers.
+ * Evitando a verbosidade abaixo:
+ * @example
+ * try {
+ *  //...
+ * } catch (error) {
+ *  next(error)
+ * }
+ */
+exports.asyncHandler = fnHandler =>
+  (req, res, next = console.error) => Promise.resolve( fnHandler(req, res) ).catch(next);
