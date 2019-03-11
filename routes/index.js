082dcc6d5054f182da484c2a7bceddb65feeb22b
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-03-11 16:21:31 -0400

    add: middleware para validar conexão do BD na /api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 // @ts-check
 const router = require('express').Router();
+const { isConnected } = require('../db');
+
+router.use('/calcs', require('./calcs'));
+
+// middleware para verificar o estado do BD, que será usada por `/api`
+router.use((req, res, next) => {
+  if (isConnected()) return next();
+  throw new Error(`Database is not connected`);
+});
 
 router.use('/api', require('./api'));
-router.use('/calcs', require('./calcs'));
 
 module.exports = router;
